632f585eb4931f8f135e4d1ddb64ee5269a4fe8b
serwmwser <serwmwser@gmail.com>
2026-04-09 00:03:28 +0300

    fix: add missing react-chessboard dependency

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,26 +6,23 @@ import { metaMask } from 'wagmi/connectors'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
 
-export const config = createConfig({
+const config = createConfig({
   chains: [bsc, bscTestnet],
   connectors: [metaMask()],
   transports: {
     [bsc.id]: http(),
-    [bscTestnet.id]: http()
-  }
+    [bscTestnet.id]: http(),
+  },
 })
 
 const queryClient = new QueryClient()
-const rootEl = document.getElementById('root')
 
-if (rootEl) {
-  ReactDOM.createRoot(rootEl).render(
-    <React.StrictMode>
-      <WagmiProvider config={config}>
-        <QueryClientProvider client={queryClient}>
-          <App />
-        </QueryClientProvider>
-      </WagmiProvider>
-    </React.StrictMode>
-  )
-}
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </WagmiProvider>
+  </React.StrictMode>
+)
